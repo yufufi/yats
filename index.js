1d5fb41e37047a0f52f9412440d2c60255a0e8c8
Y Furkan Fidan <dev@yufufi.com>
2017-04-15 12:11:18 +0100

    Latest

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var app = express();
 
 app.param('fileName', function(request, response, next, fileName) {
     console.log("app param executed");
-    var basePath = "/home/yufufi/Documents/wiki/todo/";
+    //var basePath = "/home/yufufi/Documents/wiki/todo/";
+    var basePath = "/Users/yufufi/Documents/wiki/todo/";
     var ext = ".taskpaper";
     var taskPaperFile = basePath + fileName + ext;
     var fileContent = fs.readFileSync(taskPaperFile, { encoding: "utf8" });
@@ -22,10 +23,18 @@ app.get('/notes', function(req, res) {
 });
 
 app.get('/tp/:fileName', function(req, res) {
-    console.log("Reqest received");
+    if (!req.taskpaper) {
+        res.json({ error: "No such paper found" });
+        return;
+    }
+    debugger;
     var js = JSON.stringify(req.taskpaper, null, 2);
+    var names = [];
+    req.taskpaper.children.forEach(function(element) {
+        names.push(element.getName());
+    });
     console.log(js);
-    res.json(js);
+    res.json(names);
     console.log("Done with the request");
 });
 
